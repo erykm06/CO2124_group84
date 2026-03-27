73a2e23f11780864ecc2768e74fa52385cc65363
erykm06 <erykmazurekk@gmail.com>
2026-03-27 15:54:30 +0000

    Setting up part 1 build

---
+++ part1/HQ-Administration-API/src/main/java/com/example/hqadministrationapi/HqAdministrationApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.hqadministrationapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HqAdministrationApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HqAdministrationApiApplication.class, args);
+    }
+
+}
---
+++ part1/HQ-Administration-API/src/test/java/com/example/hqadministrationapi/HqAdministrationApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hqadministrationapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HqAdministrationApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
